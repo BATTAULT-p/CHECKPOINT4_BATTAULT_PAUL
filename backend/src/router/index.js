5b23b1d4365a9d42c35bbcd9cbd92244017ca638
paul battault <paulbattault@gmail.com>
2023-02-01 12:30:43 +0100

    mvc admin ok

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
 const router = express.Router();
+const adminRouter = require("./adminRouter");
+
+router.use("/admin", adminRouter);
 
 module.exports = router;
